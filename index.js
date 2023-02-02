be1cab4e90f0b497a8f109b1ecb1e4d814c7fc55
Kingshuk Majumder <kingshukm2021@gmail.com>
2023-02-02 09:30:14 +0530

    Mongodb connection with static data in Mongoose

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hi Mongoose");
+const mongoose=require('mongoose');
+const url='mongodb://localhost:27017/e-comm';
+
+const main=async ()=>{
+    await mongoose.connect(url);
+    const productSchema=new mongoose.Schema({
+        item:String,
+        brand:String
+    });
+    const productModel=mongoose.model('products',productSchema);
+    const data=new productModel({item:'S-20',brand:'Samsung'});
+    const result=await data.save();
+    console.log(result);
+
+}
+
+main();
